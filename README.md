b685e2ed21a8c4b264733695da230251786dfcfc
Fxtradersline <fxtraderhelpline@gmail.com>
2024-12-24 23:01:23 +0530

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 "# Fxtrader" 
 "# Fxtrader" 
 "# Fxtrader" 
+"# FXTraders" 
--- market/settings.py
+++ market/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-u)4xq%mk_=@h+7=!@kyj2nc@x00a(m%c&gh=vkhq^c+2vgdhw*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.vercel.app',
+    '.now.sh'
+]
 
 
 # Application definition
@@ -36,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-   
     'django.contrib.staticfiles',
     'home',
 ]
@@ -118,12 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
-STATIC_ROOT =os.path.join(BASE_DIR,'assests')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'staticfiles_build','media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
