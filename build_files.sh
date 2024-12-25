5dcf962722e047dba808b322860b3e9a09bc5643
Fxtradersline <fxtraderrhelpline@gmail.com>
2024-12-25 16:17:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-pip3 install -r requirements.txt
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
-python3 manage.py runserver
+# #!/bin/bash
+# pip3 install -r requirements.txt
+# python3 manage.py collectstatic --no-input
+# python3 manage.py migrate
+# python3 manage.py runserver
